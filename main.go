58171a58f2c599ace6fc9120fef2ae5ba387bf1b
natitbc <nati@techbase.camp>
2024-04-23 01:19:00 +0700

    server | get tax

--- main.go
+++ main.go
@@ -15,12 +15,32 @@ type User struct {
 	Age  int    `json:"age"`
 }
 
+type Tax struct {
+	Tax float64 `json:"tax"`
+}
+
 type Err struct {
 	Message string `json:"message"`
 }
 
 var users = []User{
-	{ID: 1, Name: "AnuchitO", Age: 18},
+	{ID: 1, Name: "AnuchitO", Age: 20},
+}
+
+var tax = []Tax{
+	{Tax: 29000.0},
+}
+
+func createTaxHandler(c echo.Context) error {
+	t := Tax{}
+	err := c.Bind(&t)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+	}
+	tax = append(tax, t)
+	fmt.Println("id : % #v\n", t)
+	return c.JSON(http.StatusCreated, t)
+
 }
 
 func createUserHandler(c echo.Context) error {
@@ -35,6 +55,11 @@ func createUserHandler(c echo.Context) error {
 	fmt.Println("id : % #v\n", u)
 
 	return c.JSON(http.StatusCreated, u)
+}
+
+func getTaxHandler(c echo.Context) error {
+	fmt.Print("tax : % #v\n", tax)
+	return c.JSON(http.StatusOK, tax)
 }
 
 func getUsersHandler(c echo.Context) error {
@@ -55,7 +80,9 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.POST("/users", createUserHandler)
+	e.POST("/tax", createTaxHandler)
 	e.GET("/users", getUsersHandler)
+	e.GET("/tax", getTaxHandler)
 
 	log.Fatal(e.Start(":8080"))
 }
