0959f875589e85e7f469bf6d2bc03a537e986125
natitbc <nati@techbase.camp>
2024-04-24 00:04:16 +0700

    main | remove user func

--- main.go
+++ main.go
@@ -11,22 +11,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
 type Tax struct {
 	Tax float64 `json:"tax"`
 }
 
 type Err struct {
 	Message string `json:"message"`
-}
-
-var users = []User{
-	{ID: 1, Name: "AnuchitO", Age: 20},
 }
 
 var tax = []Tax{
@@ -42,30 +32,11 @@ func createTaxHandler(c echo.Context) error {
 	tax = append(tax, t)
 	fmt.Println("id : % #v\n", t)
 	return c.JSON(http.StatusCreated, t)
-
-}
-
-func createUserHandler(c echo.Context) error {
-	u := User{}
-	err := c.Bind(&u)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
-	}
-
-	users = append(users, u)
-
-	fmt.Println("id : % #v\n", u)
-
-	return c.JSON(http.StatusCreated, u)
 }
 
 func getTaxHandler(c echo.Context) error {
 	fmt.Print("tax : % #v\n", tax)
 	return c.JSON(http.StatusOK, tax)
-}
-
-func getUsersHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, users)
 }
 
 func main() {
@@ -93,9 +64,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.POST("/users", createUserHandler)
 	e.POST("/tax", createTaxHandler)
-	e.GET("/users", getUsersHandler)
 	e.GET("/tax", getTaxHandler)
 
 	log.Fatal(e.Start(":8080"))
