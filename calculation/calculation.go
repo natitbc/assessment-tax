c0ed968c0c7b25af920d520da0d656231b42385d
natitbc <nati@techbase.camp>
2024-04-25 00:25:43 +0700

    test case tax with wht

--- calculation/calculation.go
+++ calculation/calculation.go
@@ -12,17 +12,29 @@ type Allowance struct {
 
 func CalculateTax(totalIncome float64, wht float64, allowances []Allowance) (float64, error) {
 	// extract allowance
-	allowancesMap := make(map[string]float64)
-	fmt.Println(allowancesMap)
+
+	PERSONAL_ALLOWANCE := 60000.0
 
 	if wht < 0.0 {
 		return 0.0, errors.New("wht cannot be negative")
 	}
 
+	incomeAfterAllowance := totalIncome - PERSONAL_ALLOWANCE
+
 	// calculate tax
-	if (totalIncome - wht) <= 150000.0 {
+	if (incomeAfterAllowance) <= 150000.0 {
 		return 0.0, nil
 	}
 
-	return 29000.0, nil
+	incomeAfterAllowanceStep1 := incomeAfterAllowance - 150000.0
+
+	if incomeAfterAllowanceStep1 > 150000.0 && incomeAfterAllowanceStep1 <= 300000.0 {
+		fmt.Println(incomeAfterAllowanceStep1)
+
+		unpaidTax := (incomeAfterAllowanceStep1 * 0.1) - wht
+		fmt.Println(unpaidTax)
+		return unpaidTax, nil
+	}
+
+	return incomeAfterAllowance, nil
 }
--- calculation/calculation_test.go
+++ calculation/calculation_test.go
@@ -1,10 +1,13 @@
 package calculation
 
 import (
+	"math"
 	"testing"
 )
 
 func TestCalculation(t *testing.T) {
+
+	const epsilon = 0.001 // Adjust tolerance as needed
 
 	_, err := CalculateTax(150000.0, -1.0, []Allowance{})
 	if err == nil {
@@ -23,6 +26,14 @@ func TestCalculation(t *testing.T) {
 	want = 0.0
 	got, _ = CalculateTax(150000.0, 0.0, []Allowance{})
 	if got != want {
+		t.Errorf("Expected %f, got %f", want, got)
+	}
+
+	// test got tax 4000.0 when input 500000.0, 25000.0, []
+
+	want = 4000.0
+	got, _ = CalculateTax(500000.0, 25000.0, []Allowance{})
+	if math.Abs(got-want) > epsilon {
 		t.Errorf("Expected %f, got %f", want, got)
 	}
 
