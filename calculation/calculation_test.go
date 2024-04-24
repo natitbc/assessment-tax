4979d49e4e83453229cea776b2f22400c502575a
natitbc <nati@techbase.camp>
2024-04-25 02:59:00 +0700

    add test case for taxlevel

--- calculation/calculation_test.go
+++ calculation/calculation_test.go
@@ -54,4 +54,30 @@ func TestCalculation(t *testing.T) {
 		t.Errorf("Expected %f, got %f", want, got)
 	}
 
+	expectedTaxLevels := []TaxLevel{
+		{level: "0-150,000", tax: 0.0},
+		{level: "150,001-500,000", tax: 19000.0},
+		{level: "500,001-1,000,000", tax: 0.0},
+		{level: "1,000,001-2,000,000", tax: 0.0},
+		{level: "2,000,001 ขึ้นไป", tax: 0.0},
+	}
+
+	_, actualTaxLevels, _ := CalculateTax(500000.0, 0.0, []Allowance{
+		{AllowanceType: "k-receipt", Amount: 200000.0},
+		{AllowanceType: "donation", Amount: 100000.0},
+	})
+
+	if len(expectedTaxLevels) != len(actualTaxLevels) {
+		t.Errorf("Expected %d tax levels, got %d", len(expectedTaxLevels), len(actualTaxLevels))
+	}
+
+	for i := 0; i < len(expectedTaxLevels); i++ {
+		if expectedTaxLevels[i].level != actualTaxLevels[i].level {
+			t.Errorf("Expected level %s, got %s", expectedTaxLevels[i].level, actualTaxLevels[i].level)
+		}
+		if math.Abs(expectedTaxLevels[i].tax-actualTaxLevels[i].tax) > epsilon {
+			t.Errorf("Expected tax %f, got %f", expectedTaxLevels[i].tax, actualTaxLevels[i].tax)
+		}
+	}
+
 }
